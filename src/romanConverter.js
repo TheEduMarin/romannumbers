dbdda746e248162d729ee2868611000fe32fde87
Eduardo Marin Espinoza <emarine@Eduardos-Laptop.local>
2025-03-08 05:49:23 -0400

    otro commit

--- src/romanConverter.js
+++ src/romanConverter.js
@@ -1,7 +1,12 @@
 export function convertToRoman(num) {
-    if (num === 1) {
-      return 'I';
-    }
-    return '';
+  if (num === 1) {
+    return 'I';
+  } else if (num === 2) {
+    return 'II';
+  } else if (num === 5) {
+    return 'V';
+  } else if (num === 7) {
+    return 'VII';
   }
-  
+  return '';
+}
--- src/romanConverter.test.js
+++ src/romanConverter.test.js
@@ -1,5 +1,15 @@
-import { convertToRoman } from './romanConverter';
-
 test('should return "I" for 1', () => {
   expect(convertToRoman(1)).toBe('I');
+});
+
+test('should return "II" for 2', () => {
+  expect(convertToRoman(2)).toBe('II');
+});
+
+test('should return "V" for 5', () => {
+  expect(convertToRoman(5)).toBe('V');
+});
+
+test('should return "VII" for 7', () => {
+  expect(convertToRoman(7)).toBe('VII');
 });
